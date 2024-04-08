78bae05b3e4f9bc23570bd57fefe99b470595f66
Johncod24 <165402872+Johncod24@users.noreply.github.com>
2024-04-08 15:58:28 +0000

    git add . git commit -m "Resolvido conflitos e mesclado com o main"

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'johncode24/gesescpm',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5c83c353ec6eae9b5d60bcd95aa6a4b447ed698e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'johncode24/gesescpm' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5c83c353ec6eae9b5d60bcd95aa6a4b447ed698e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
