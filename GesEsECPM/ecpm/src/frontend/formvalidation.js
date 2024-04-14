79a7ebe59913d576c1bb9b7e89b5154d2c80f689
Johncod24 <165402872+Johncod24@users.noreply.github.com>
2024-04-14 15:21:59 +0000

    git add . git commit -m "Resolvido conflitos e mesclado com o main"

--- GesEsECPM/ecpm/src/frontend/formvalidation.js
+++ GesEsECPM/ecpm/src/frontend/formvalidation.js
@@ -1,54 +1,17 @@
-function validateForm() {
-    var nome = document.getElementById('nome').value;
-    var matricula = document.getElementById('matricula').value;
 
-    if (nome.trim() == "" || matricula.trim() == "") {
-        alert("Nome e matrícula são obrigatórios.");
-        return false;
-    }
-}
-// Função para exibir os resultados da consulta de alunos em um curso
-function consultarAlunosCurso() {
-    var idCurso = document.getElementById('id_curso').value;
-    var request = new XMLHttpRequest();
-    request.open('POST', '../backend/alunos_curso.php', true);
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    request.onreadystatechange = function() {
-        if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
-            document.getElementById('alunos_curso_resultado').innerHTML = request.responseText;
-        }
-    };
-    request.send('id_curso=' + idCurso);
-}
-
-// Função para exibir os resultados da consulta de alunos em uma turma
-function consultarAlunosTurma() {
-    var idTurma = document.getElementById('id_turma').value;
-    var request = new XMLHttpRequest();
-    request.open('POST', '../backend/alunos_turma.php', true);
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    request.onreadystatechange = function() {
-        if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
-            document.getElementById('alunos_turma_resultado').innerHTML = request.responseText;
-        }
-    };
-    request.send('id_turma=' + idTurma);
-}
-function validateCursoForm() {
+export function validateCursoForm() {
     var nome = document.getElementById('nome').value;
     var codigo = document.getElementById('codigo').value;
-    var cargaHoraria = document.getElementById('carga_horaria').value;
+    var carga_horaria = document.getElementById('carga_horaria').value;
 
-    if (nome.trim() === "" || codigo.trim() === "" || cargaHoraria.trim() === "") {
+    if (nome === "" || codigo === "" || carga_horaria === "") {
         alert("Nome, código e carga horária são obrigatórios.");
         return false;
     }
-
-    // Adicione mais validações conforme necessário
-
     return true;
 }
-function validateProfessorForm() {
+
+export function validateProfessorForm() {
     var nome = document.getElementById('nome').value;
     var disciplinas = document.getElementById('disciplinas').value;
 
@@ -58,7 +21,8 @@ function validateProfessorForm() {
     }
     return true;
 }
-function validateRegistrationForm() {
+
+export function validateRegistrationForm() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
@@ -68,7 +32,8 @@ function validateRegistrationForm() {
     }
     return true;
 }
-function validateDisciplineForm() {
+
+export function validateDisciplineForm() {
     var nome = document.getElementById('nome').value;
     var codigo = document.getElementById('codigo').value;
     var cargaHoraria = document.getElementById('carga_horaria').value;
@@ -80,7 +45,7 @@ function validateDisciplineForm() {
     return true;
 }
 
-function validateLoginForm() {
+export function validateLoginForm() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
@@ -91,7 +56,7 @@ function validateLoginForm() {
     return true;
 }
 
-function validateTurmaForm() {
+export function validateTurmaForm() {
     var id_disciplina = document.getElementById('id_disciplina').value;
     var id_professor = document.getElementById('id_professor').value;
     var horario = document.getElementById('horario').value;
@@ -104,7 +69,7 @@ function validateTurmaForm() {
     return true;
 }
 
-function validateAlunoForm() {
+export function validateAlunoForm() {
     var nome = document.getElementById('nome').value;
     var matricula = document.getElementById('matricula').value;
     var nascimento = document.getElementById('nascimento').value;
@@ -117,7 +82,7 @@ function validateAlunoForm() {
     return true;
 }
 
-function validateMatriculaForm() {
+export function validateMatriculaForm() {
     var id_aluno = document.getElementById('id_aluno').value;
     var id_curso = document.getElementById('id_curso').value;
 
--- GesEsECPM/ecpm/src/index.js
+++ GesEsECPM/ecpm/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { validateCursoForm, validateAlunoForm, validateDisciplineForm, validateLoginForm, validateMatriculaForm, validateProfessorForm, validateRegistrationForm, validateTurmaForm } from '../src/frontend/formvalidation';
+import { consultarAlunosCurso, consultarAlunosTurma } from "../src/frontend/formconsultar";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,76 +17,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-function validateForm() {
-  var nome = document.getElementById('nome').value;
-  var matricula = document.getElementById('matricula').value;
-
-  if (nome.trim() == "" || matricula.trim() == "") {
-      alert("Nome e matrícula são obrigatórios.");
-      return false;
-  }
-}
-// Função para exibir os resultados da consulta de alunos em um curso
-function consultarAlunosCurso() {
-  var idCurso = document.getElementById('id_curso').value;
-  var request = new XMLHttpRequest();
-  request.open('POST', '../backend/alunos_curso.php', true);
-  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-  request.onreadystatechange = function() {
-      if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
-          document.getElementById('alunos_curso_resultado').innerHTML = request.responseText;
-      }
-  };
-  request.send('id_curso=' + idCurso);
-}
-
-// Função para exibir os resultados da consulta de alunos em uma turma
-function consultarAlunosTurma() {
-  var idTurma = document.getElementById('id_turma').value;
-  var request = new XMLHttpRequest();
-  request.open('POST', '../backend/alunos_turma.php', true);
-  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-  request.onreadystatechange = function() {
-      if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
-          document.getElementById('alunos_turma_resultado').innerHTML = request.responseText;
-      }
-  };
-  request.send('id_turma=' + idTurma);
-}
-function validateTurmaForm() {
-  var id_disciplina = document.getElementById('id_disciplina').value;
-  var id_professor = document.getElementById('id_professor').value;
-  var horario = document.getElementById('horario').value;
-  var sala_de_aula = document.getElementById('sala_de_aula').value;
-
-  if (id_disciplina === "" || id_professor === "" || horario === "" || sala_de_aula === "") {
-      alert("ID da disciplina, ID do professor, horário e sala de aula são obrigatórios.");
-      return false;
-  }
-  return true;
-}
-
-function validateAlunoForm() {
-  var nome = document.getElementById('nome').value;
-  var matricula = document.getElementById('matricula').value;
-  var nascimento = document.getElementById('nascimento').value;
-  var endereco = document.getElementById('endereco').value;
-
-  if (nome === "" || matricula === "" || nascimento === "" || endereco === "") {
-      alert("Nome, matrícula, data de nascimento e endereço são obrigatórios.");
-      return false;
-  }
-  return true;
-}
-
-function validateMatriculaForm() {
-  var id_aluno = document.getElementById('id_aluno').value;
-  var id_curso = document.getElementById('id_curso').value;
-
-  if (id_aluno === "" || id_curso === "") {
-      alert("ID do aluno e ID do curso são obrigatórios.");
-      return false;
-  }
-  return true;
-}
